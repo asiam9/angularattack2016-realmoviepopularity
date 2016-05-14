a2133617ad279ffa29eea308aaa3f5933739a42d
Evgeny Leychenok <e.leychenok@gmail.com>
2016-05-13 21:04:35 -0700

    Migrated to Typescript

---
+++ app/containers/app.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Real Movie Popularity</h1><p>This project runs!</p>'
+})
+export class AppComponent {
+    
+}
--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
-(function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic.bootstrap(app.AppComponent);
-  });
-})(window.app || (window.app = {}));
+"use strict";
+var core_1 = require('@angular/core');
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_1 = require('./containers/app');
+core_1.enableProdMode();
+platform_browser_dynamic_1.bootstrap(app_1.AppComponent);
+//# sourceMappingURL=main.js.map
