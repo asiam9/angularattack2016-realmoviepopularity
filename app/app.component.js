760707cf066ba1ada2bb4dedfaf1b95fd62d73a2
Evgeny Leychenok <e.leychenok@gmail.com>
2016-05-13 20:40:23 -0700

    Bootstrapping the app

--- app/app.component.js
+++ app/app.component.js
@@ -2,7 +2,7 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>AngularAttack 2016</h1><p>Your project runs!</p>'
+      template: '<h1>Real Movie Popularity</h1><p>This project runs!</p>'
     })
     .Class({
       constructor: function() {}
